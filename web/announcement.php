bc1fbb82ede2f05d9d8b4e47fbd2815915442698
whes1015 <44525760+whes1015@users.noreply.github.com>
2021-09-09 01:41:39 +0800

    Create announcement.php

---
+++ web/announcement.php
@@ -0,0 +1,113 @@
+<p>&nbsp;</p> 
+
+<p>&nbsp;</p> 
+
+<p style="margin-left:40px"><span style="font-family:Verdana,Geneva,sans-serif"><span style="color:#ffffff"><span style="font-size:2rem">
+
+• 21.06.16 下午9:00 - <font color=red>［錯誤 下午9:05修復］</font>網站因端口衝突導致的403無權限訪問
+
+<br>
+
+<br>
+
+• 21.06.09 上午9:05 - <font color=#2894FF>［公告］</font>服務器維護完畢，目前全部功能正常運行
+
+<br>
+
+<br>
+
+• 21.06.08 上午12:27 - <font color=#2894FF>［公告］</font>所有設備已維護完畢，全部服務器正常運行
+
+<br>
+
+<br>
+
+• 21.06.07 下午1:33 - <font color=red>［錯誤 下午11:15修復］</font>更換Minecraft服務器軟體，由原本的php架構改成Java，提升流暢度
+
+<br>
+
+<br>
+
+• 21.06.07 下午12:33 - <font color=red>［錯誤 下午6:12修復］</font>因未知原因導致服務器系統檔案損毀
+
+<br>
+
+<br>
+
+• 21.06.06 下午2:32 - <font color=red>［錯誤 下午2:42修復］</font>網站申請SSL憑證，可能導致過程中無法訪問
+
+<br>
+
+<br>
+
+• 21.06.05 下午10:50 - <font color=#FF9224>［優化 下午11:00完成］</font>為提高服務器性能，對服務器進行超頻，過程中可能導致部分服務無法使用
+
+<br>
+
+<br>
+
+• 21.06.05 下午7:50 - <font color=#FF9224>［優化 下午8:40完成］</font>改善服務器設備散熱，過程中可能會出現網路波動或服務器離線
+
+<br>
+
+<br>
+
+• 21.06.05 上午3:50 - <font color=red>［錯誤 上午10:40修復］</font>中華電信固網電信IP異動，導致服務器離線，已申請固定IP來排除錯誤
+
+<br>
+
+<br>
+
+• 21.06.04 下午9:58 - <font color=#FF9224>［優化 下午10:48完成］</font>改善服務器設備散熱，優化延遲問題，提升數據吞吐量
+
+<br>
+
+<br>
+
+• 21.06.04 下午9:02 - <font color=red>［錯誤 下午9:15修復］</font>圖片檔案丟失導致頁面顯示異常
+
+<br>
+
+<br>
+
+• 21.06.04 下午7:39 - <font color=#28FF28>［新增］</font>上傳最新版本Minecraft (1.16.221.01)Android客戶端安裝檔至服務器
+
+<br>
+
+<br>
+
+• 21.06.04 下午6:41 - <font color=red>［錯誤 下午6:46修復］</font>編輯服務器配置文件導致服務器脫機
+
+<br>
+
+<br>
+
+• 21.06.04 下午5:58 - <font color=red>［錯誤 下午6:01修復］</font>設備登入權限異常，服務器關機重啟
+
+<br>
+
+<br>
+
+• 21.06.04 上午10:00 - <font color=#2894FF>［公告］</font>所有設備已維護完畢，全部服務器正常運行
+
+</p> 
+
+<style>
+
+body{
+
+margin:0px;
+
+padding:0px;
+
+background:#fff url(http://220.134.162.44/download/home.jpeg) center center fixed no-repeat;　//設定背景圖片的呈現方式
+
+width: 100%;
+
+height:100%;
+
+background-size: cover;　
+
+}        
+
+</style>
