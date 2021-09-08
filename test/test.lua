a038dce71250b437e6d83eb2b6277411195ee25a
whes1015 <44525760+whes1015@users.noreply.github.com>
2021-09-09 02:20:44 +0800

    Update test.lua

--- test/test.lua
+++ test/test.lua
@@ -1,1 +1,13 @@
+function Point(x, y)        -- "Point" object constructor
 
+  return { x = x, y = y }   -- Creates and returns a new object (table)
+
+end
+
+array = { Point(10, 20), Point(30, 40), Point(50, 60) }   -- Creates array of points
+
+for i = 1,3 do 
+
+  print("point("..array[i].x..","..array[i].y..")")
+
+end
